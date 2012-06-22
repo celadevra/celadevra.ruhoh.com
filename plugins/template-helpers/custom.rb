bb49362ba9fc32ab64b90f2bfaef58d094b27630
Celad Evra <snakehsu@gmail.com>
2012-06-22 10:39:26 +0800

    Make the navbar "brand" recognize custom wwwroot

---
+++ plugins/template-helpers/custom.rb
@@ -0,0 +1,10 @@
+class Ruhoh
+        module Templaters
+                module Helpers
+                        def docroot
+                                docroot = "#{Ruhoh.urls.docroot}"
+                                docroot
+                        end
+                end
+        end
+end

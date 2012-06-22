69cf7e76f563758980883e929ca8b0b1d215b6d8
Celad Evra <snakehsu@gmail.com>
2012-06-22 14:10:46 +0800

    Fix faulty brand link on navbar

--- plugins/template-helpers/custom.rb
+++ plugins/template-helpers/custom.rb
@@ -2,7 +2,11 @@ class Ruhoh
         module Templaters
                 module Helpers
                         def docroot
-                                docroot = "#{Ruhoh.urls.docroot}"
+                                docroot =
+                                        self.context['site']['config']['environment']
+                                == 'production' ?
+                                        self.context['site']['config']['production_url']
+                                : self.context['site']['config']['dev_url']
                                 docroot
                         end
                 end

edfd015dba49deafb2e5f40854438745ec3a13ea
Celad Evra <snakehsu@gmail.com>
2012-06-22 14:23:56 +0800

    Fix faulty brand link
    And also allow generate brand link depend on dev or production
    environment.

--- plugins/template-helpers/custom.rb
+++ plugins/template-helpers/custom.rb
@@ -2,11 +2,7 @@ class Ruhoh
         module Templaters
                 module Helpers
                         def docroot
-                                docroot =
-                                        self.context['site']['config']['environment']
-                                == 'production' ?
-                                        self.context['site']['config']['production_url']
-                                : self.context['site']['config']['dev_url']
+                                docroot = self.context['site']['config']['env'] == 'production' ?  self.context['site']['config']['production_url'] : self.context['site']['config']['dev_url']
                                 docroot
                         end
                 end

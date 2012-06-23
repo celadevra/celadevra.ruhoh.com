c6ce1a26a4fc2a35175f5dcac8d27cb4464bbeab
Celad Evra <snakehsu@gmail.com>
2012-06-23 23:03:42 +0800

    Reindented the file in Emacs

--- plugins/template-helpers/custom.rb
+++ plugins/template-helpers/custom.rb
@@ -1,11 +1,10 @@
 class Ruhoh
-        module Templaters
-                module Helpers
-                        def docroot
-                                docroot = self.context['site']['config']['env'] == 'production' ?  self.context['site']['config']['production_url'] : self.context['site']['config']['dev_url']
-                                docroot ||= '/'
-                                docroot
-                        end
-                end
-        end
-end
+  module Templaters
+    module Helpers
+      def docroot
+        docroot = self.context['site']['config']['env'] == 'production' ?
+        self.context['site']['config']['production_url'] :
+        self.context['site']['config']['dev_url']
+        docroot ||= '/'
+        docroot
+      end

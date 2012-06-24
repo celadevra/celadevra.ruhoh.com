12daa6c432b49f9202b21808ee679aa339e41833
Celad Evra <snakehsu@gmail.com>
2012-06-24 13:43:20 +0800

    Optimize custom docroot

--- plugins/template-helpers/custom.rb
+++ plugins/template-helpers/custom.rb
@@ -2,9 +2,7 @@ class Ruhoh
   module Templaters
     module Helpers
       def docroot
-        docroot = self.context['site']['config']['env'] == 'production' ?
-        self.context['site']['config']['production_url'] :
-          self.context['site']['config']['dev_url']
+        docroot = self.context['site']['config']['env'] == 'production' ? self.context['site']['config']['production_url'] : Ruhoh::DB.site['config']['dev_url']
         docroot ||= '/'
         docroot
       end
